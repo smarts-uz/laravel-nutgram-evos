c4933f637eadd476c13688818c783b28a781f6be
abror9603 <avazovabror1996@gmail.com>
2023-07-10 09:06:08 +0500

    add class in HandlersClass

---
+++ app/Telegram/InlineButtons/Handlers/HandlersPriorityClass.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ *
+ * Author:  Asror Zakirov
+ * https://www.linkedin.com/in/asror-zakirov
+ * https://github.com/asror-z
+ *
+ */
+
+namespace App\Telegram\InlineButtons\Handlers;
+
+use SergiX44\Nutgram\Nutgram;
+
+class HandlersPriorityClass
+
+{
+    public function __invoke(Nutgram $bot):void
+    {
+        $bot->answerCallbackQuery([
+            'text' => 'Darslar tez kunda yuklanadi'
+        ]);
+    }
+}
